1840717de7ab70a43d2e00e7ae7f1c5837fef4b4
joverixal <joverixal.entuna@gmail.com>
2026-06-01 15:33:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://bqbiufqfnulywbheossb.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://bqbiufqfnulywbheossb.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxYml1ZnFmbnVseXdiaGVvc3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyOTM2OTIsImV4cCI6MjA5NTg2OTY5Mn0.mZ6dWa2sc811Y2_rmBZEFvLK9bpCfvouKh22wZqLEnE";
 
 const headers = {
@@ -10,7 +10,7 @@ const headers = {
 // ➕ Insert customer
 $("#saveBtn").click(function () {
   $.ajax({
-    url: SUPABASE_URL + "users",
+    url: SUPABASE_URL + "/rest/v1/users?select=*",
     method: "POST",
     headers: headers,
     data: JSON.stringify({
